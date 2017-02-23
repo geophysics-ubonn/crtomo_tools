3086338c1d11700670c6db3cb8208f5d0e1bdba4
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2017-02-23 17:14:21 +0100

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,21 @@
 #!/usr/bin/env python
 from setuptools import setup
+import os
 import glob
-# from setuptools import find_packages
-# find_packages
-
-# under windows, run
-# python.exe setup.py bdist --format msi
-# to create a windows installer
 
 scripts = glob.glob('src/*.py')
-# scripts = ['src/GRID_TOOLS/td_test.py', ]
-# scripts = [],
 
 version_short = '0.1'
 version_long = '0.1.0'
 
 # generate entry points
 entry_points = {'console_scripts': []}
+scripts = [os.path.basename(script)[0:-3] for script in glob.glob('src/*.py')]
+for script in scripts:
+    print(script)
+    entry_points['console_scripts'].append(
+        '{0} = {0}:main'.format(script)
+    )
 
 
 if __name__ == '__main__':
@@ -28,11 +27,12 @@ if __name__ == '__main__':
         license='GPL-3',
         author_email='mweigand@geo.uni-bonn.de',
         url='http://www.geo.uni-bonn.de/~mweigand',
-        entry_points={
-            'console_scripts': [
-                'td_test = td_test:main',
-            ],
-        },
+        entry_points=entry_points,
+        # entry_points={
+        #     'console_scripts': [
+        #         'td_test = td_test:main',
+        #     ],
+        # },
         # package_dir={'': 'src/', 'crtomo': 'lib/crtomo'},
         package_dir={
             '': 'src',
@@ -42,15 +42,13 @@ if __name__ == '__main__':
         # package_dir={'': 'lib', 'grid_tools': 'src/GRID_TOOLS'},
         # packages=find_packages(),
         packages=['crtomo', ],
-        py_modules=[
-            'td_test',
-        ],
+        py_modules=scripts,
         # py_modules=[
         #     splitext(basename(i))[0] for i in glob.glob("src/*.py")
         # ]
         # packages=[
         #     'crtomo',
         # ],
-        scripts=scripts,
+        # scripts=scripts,
         # install_requires=['numpy', 'scipy', 'matplotlib'],
     )
