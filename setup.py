37469c2b38805c311d9e2d44eb4066572673381d
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2017-02-23 17:07:50 +0100

    work on a unified setup for Windows/Linux

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 from setuptools import setup
-from setuptools import find_packages
-# import glob
+import glob
 # from setuptools import find_packages
 # find_packages
 
@@ -9,9 +8,9 @@ from setuptools import find_packages
 # python.exe setup.py bdist --format msi
 # to create a windows installer
 
-# scripts = glob.glob('src/GRID_TOOLS/*.py')
+scripts = glob.glob('src/*.py')
 # scripts = ['src/GRID_TOOLS/td_test.py', ]
-scripts = [],
+# scripts = [],
 
 version_short = '0.1'
 version_long = '0.1.0'
@@ -34,18 +33,24 @@ if __name__ == '__main__':
                 'td_test = td_test:main',
             ],
         },
-        # find_packages() somehow does not work under Win7 when creating a
-        # msi # installer
-        package_dir={'': 'src/', 'crtomo': 'lib/crtomo'},
+        # package_dir={'': 'src/', 'crtomo': 'lib/crtomo'},
+        package_dir={
+            '': 'src',
+            'crtomo': 'lib/crtomo'
+        },
         # packages=[''],
         # package_dir={'': 'lib', 'grid_tools': 'src/GRID_TOOLS'},
-        packages=find_packages(),
+        # packages=find_packages(),
+        packages=['crtomo', ],
+        py_modules=[
+            'td_test',
+        ],
         # py_modules=[
         #     splitext(basename(i))[0] for i in glob.glob("src/*.py")
         # ]
         # packages=[
         #     'crtomo',
         # ],
-        # scripts=scripts,
+        scripts=scripts,
         # install_requires=['numpy', 'scipy', 'matplotlib'],
     )
