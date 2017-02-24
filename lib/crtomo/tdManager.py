1a196d21b60fb6510031a78402cc2265689ce7ee
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2017-02-24 15:35:03 +0100

    work on tdManager

--- lib/crtomo/tdManager.py
+++ lib/crtomo/tdManager.py
@@ -4,15 +4,18 @@
 
 """
 import os
-# import crtomo.nodeManager as nM
-# import crtomo.parManager as pM
+
+import crtomo.grid as CRGrid
+import crtomo.nodeManager as nM
+import crtomo.parManager as pM
+import crtomo.configManager as cConf
 
 
 class tdMan(object):
     """Manage tomodirs
 
     """
-    def __init__(self, kwargs):
+    def __init__(self, **kwargs):
         """The following initialization permutations are possible:
 
             * initialize empty
@@ -23,6 +26,11 @@ class tdMan(object):
                 * crmod_cfg: crtomo.cfg.crmod_cfg instance
                 * crtomo_cfg: crmod.cfg.crtomo_cfg instance
 
+        Parameters
+        ----------
+
+        grid: crtomo.grid.crt_grid
+            A fully initialized grid object
         """
         # these variables will be filled later
         self.grid = None
@@ -30,9 +38,33 @@ class tdMan(object):
         self.crtomo_cfg = None
         self.nodeman = None
         self.parman = None
-        # configs
-        # measurements
+        self.configs = None
         # we need a struct to organize the assigments
+
+        # load/assign grid
+        if 'grid' in kwargs:
+            self.grid = kwargs.get('grid')
+        elif 'elem_file' in kwargs and 'elec_file' in kwargs:
+            grid = CRGrid.crt_grid()
+            grid.load_grid(
+                kwargs['elem_file'],
+                kwargs['elec_file'],
+            )
+            self.grid = grid
+        else:
+            raise Exception(
+                'You must provide either a grid instance or ' +
+                'elem_file/elec_file file paths'
+            )
+
+        parman = kwargs.get('parman', pM.ParMan(self.grid))
+        self.parman = parman
+
+        nodeman = kwargs.get('nodeman', nM.NodeMan(self.grid))
+        self.nodeman = nodeman
+
+        config = kwargs.get('configs', cConf.ConfigManager())
+        self.config = config
 
     def create_tomodir(self, directory):
         """Create a tomodir subdirectory structure in the given directory
@@ -49,3 +81,19 @@ class tdMan(object):
         for directory in directories:
             if not os.path.isdir(directory):
                 os.makedirs(directory)
+
+    def save_to_tomodir(self, directory):
+        """Save the tomodir instance to a directory structure.
+
+        Test cases:
+
+            * modeling only
+            * inversion only
+            * modeling and inversion
+
+        """
+        self.create_tomodir(directory)
+
+        # determine which set to save (modeling and/or inversion)
+        # requirements for modeling: grid, configs, rho
+        # requirements for inversion: grid, measurements
