8c2ee388cf7a6f13bd27eda62befb5a5d7b3c92f
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2017-03-06 14:25:07 +0100

    work on tests for grid_homogenize.py

--- src/TESTS/grid_homogenize/01/test.sh
+++ src/TESTS/grid_homogenize/01/test.sh
@@ -2,7 +2,8 @@
 
 rm *.png
 test -d grid_hom && rm -r grid_hom
-grid_homogenize.py -d data --dx 50 --dy 100 -o grid_hom
+
+grid_homogenize -d data --dx 50 --dy 100 -o grid_hom
 cp char_length.dat grid_hom/
 cd grid_hom
 # cr_trig_create.py grid
