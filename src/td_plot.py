05db610893f9c2fe50159b067051b90e8d8a898f
Jasmin Budler <budler@geo.uni-bonn.de>
2017-10-25 10:14:49 +0200

    pseudocode

---
+++ src/td_plot.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+Has to be run in tomodir
+END DOCUMENTATION
+'''
+import numpy as np
+import os
+from optparse import OptionParser
+import scipy as sc
+import crtomo.plotManager as CRPlot
+import crtomo.grid as CRGrid
+import pylab as plt
+import matplotlib
+
+
+# handle base options
+# handle options for mag
+    # linear, ctiks, cmin, cmax, cname
+# handle options for phase
+    # linear, ctiks, cmin, cmax, cname
+# handle oprions for real
+    # linear, ctiks, cmin, cmax, cname
+# handle options for imag
+    # linear, ctiks, cmin, cmax, cname
+
+
+# characterize tomodir
+# list files
+
+
+# read in coverage
+# read in mag, pha
+
+# calculate real
+# calculate imag
+
+# plot content corresponding to identifiers, use plots below, return fig, ax
+# plot mag
+# plot phase
+# plot cov
+# plot real
+# plot imag
+# plot overview
+    # load grid
+    # load plotmanager
+    # plot content
+
+
+def main():
+    # options
+    # list files
+    # read files
+    # plot content
+    # plot overview
+
+
+if __name__ == '__main__':
+    main()
