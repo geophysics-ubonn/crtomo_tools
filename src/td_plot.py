dd45049ab5f3d7d99c31ed88c0483ba8e14b9a56
Jasmin Budler <budler@geo.uni-bonn.de>
2017-10-25 10:16:08 +0200

    list datafiles

--- src/td_plot.py
+++ src/td_plot.py
@@ -97,8 +97,57 @@ def handle_base_options():
     # linear, ctiks, cmin, cmax, cname
 
 
-# characterize tomodir
-# list files
+def read_iter(use_fpi):
+    '''Return the path to the final .mag file either for the complex or the fpi
+    inversion.
+    '''
+    filename_rhosuffix = 'exe/inv.lastmod_rho'
+    filename = 'exe/inv.lastmod'
+    # filename HAS to exist. Otherwise the inversion was not finished
+    if(not os.path.isfile(filename)):
+        print('Inversion was not finished! No last iteration found.')
+
+    if(use_fpi is True):
+        if(os.path.isfile(filename_rhosuffix)):
+            filename = filename_rhosuffix
+
+    linestring = open(filename, 'r').readline().strip()
+    linestring = linestring.replace('\n', '')
+    linestring = linestring.replace('../', '')
+    return linestring
+
+
+def list_datafiles():
+    '''Get the type of the tomodir and the highest iteration to list all files,
+    which will be plotted.
+    '''
+    # get type of the tomodir
+    cfg = np.genfromtxt('exe/crtomo.cfg',
+                        skip_header=15,
+                        dtype='str',
+                        usecols=([0]))
+    is_complex = False
+    if cfg[0] == 'F':
+        is_complex = True
+    is_fpi = False
+    if cfg[2] == 'T':
+        is_fpi = True
+    # get the highest iteration
+    it_rho = read_iter(is_fpi)
+    it_phase = read_iter(False)
+    # list the files
+    files = ['inv/coverage.mag']
+    dtype = ['cov']
+    files.append(it_rho)
+    dtype.append('mag')
+    if is_complex:
+        files.append(it_rho.replace('mag', 'pha'))
+        dtype.append('pha')
+    if is_fpi:
+        files.append(it_phase.replace('mag', 'pha'))
+        dtype.append('pha_fpi')
+
+    return files, dtype
 
 
 # read in coverage
@@ -121,7 +170,7 @@ def handle_base_options():
 
 def main():
     b_options = handle_base_options()
-    # list files
+    [datafiles, filetype] = list_datafiles()
     # read files
     # plot content
     # plot overview
