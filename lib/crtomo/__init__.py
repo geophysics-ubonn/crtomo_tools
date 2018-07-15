6b7c89e8a49c6e673914592128c6a60c252f175c
EIT5 user <mweigand@geo.uni-bonn.de>
2018-07-15 12:57:12 +0200

    import plotManager in crtomo:
    crtomo.plotManager

--- lib/crtomo/__init__.py
+++ lib/crtomo/__init__.py
@@ -1,5 +1,6 @@
 from crtomo.tdManager import tdMan
 from crtomo.tdManager import noise_model
 from crtomo.grid import crt_grid
+from crtomo.plotManager import plotManager
 
-__all__ = ['tdMan', 'crt_grid', 'noise_model']
+__all__ = ['plotManager', 'tdMan', 'crt_grid', 'noise_model']
