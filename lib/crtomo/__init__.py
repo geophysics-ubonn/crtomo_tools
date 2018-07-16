bfc475b778956824720d5c1af60b52c5121abae6
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2018-07-16 17:06:45 +0200

    rename an imported module to prevent naming clashes

--- lib/crtomo/__init__.py
+++ lib/crtomo/__init__.py
@@ -1,6 +1,6 @@
 from crtomo.tdManager import tdMan
 from crtomo.tdManager import noise_model
 from crtomo.grid import crt_grid
-from crtomo.plotManager import plotManager
+from crtomo.plotManager import plotManager as pltMan
 
-__all__ = ['plotManager', 'tdMan', 'crt_grid', 'noise_model']
+__all__ = ['pltMan', 'tdMan', 'crt_grid', 'noise_model']
