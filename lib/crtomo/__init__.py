ddf9939677bf60f4a2a7f819591dc72194c4e15e
EIT5 user <mweigand@geo.uni-bonn.de>
2018-07-20 12:45:44 +0200

    add global import of eitMan to crtomo

--- lib/crtomo/__init__.py
+++ lib/crtomo/__init__.py
@@ -2,5 +2,6 @@ from crtomo.tdManager import tdMan
 from crtomo.tdManager import noise_model
 from crtomo.grid import crt_grid
 from crtomo.plotManager import plotManager as pltMan
+from crtomo.eitManager import eitMan
 
-__all__ = ['pltMan', 'tdMan', 'crt_grid', 'noise_model']
+__all__ = ['pltMan', 'tdMan', 'eitMan', 'crt_grid', 'noise_model']
