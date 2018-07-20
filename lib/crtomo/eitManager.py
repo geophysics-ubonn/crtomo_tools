60cf69256ed88f6e2f4dddcf31508f611b8661ac
EIT5 user <mweigand@geo.uni-bonn.de>
2018-07-20 12:46:03 +0200

    work on eitMan

--- lib/crtomo/eitManager.py
+++ lib/crtomo/eitManager.py
@@ -2,16 +2,14 @@
 """A digital representation of a sEIT inversion directory.
 
 A sEIT-dir (or sipdir) has the following structure:
-
-
-
 """
 import os
 from glob import glob
 
+import pandas as pd
 import numpy as np
 
-import crtomo.grid as CRgrid
+from crtomo.grid import crt_grid
 import crtomo.cfg as CRcfg
 import crtomo.tdManager as CRman
 
@@ -42,24 +40,6 @@ class eitMan(object):
             if given, then try to load data from this directory. Expect a
             'frequencies.dat' file and corresponding 'volt_*.dat' files.
         """
-        # load/assign grid
-        if 'grid' in kwargs:
-            self.grid = kwargs.get('grid')
-        elif 'elem_file' in kwargs and 'elec_file' in kwargs:
-            grid = CRgrid.crt_grid()
-            grid.load_grid(
-                kwargs['elem_file'],
-                kwargs['elec_file'],
-            )
-            self.grid = grid
-        else:
-            raise Exception(
-                'You must provide either a grid instance or ' +
-                'elem_file/elec_file file paths'
-            )
-
-        self.frequencies = kwargs.get('frequencies', None)
-
         # the following variables will be partially duplicated in the tomodir
         # instances down below. Thus, they are used primarily to store blue
         # prints or "parent" files for the various frequencies. For example:
@@ -72,6 +52,44 @@ class eitMan(object):
 
         # for each frequency we have a separate tomodir object
         self.tds = {}
+        # when we load data from inversion results, we need to store the
+        # corresponding parameter ids for the various quantities
+        self.assigments = {}
+        # shortcut
+        self.a = self.assigments
+
+        self.frequencies = kwargs.get('frequencies', None)
+
+        # # now load data/initialize things
+
+        seit_dir = kwargs.get('seitdir', None)
+        if seit_dir is not None:
+            # load the grid from the first invmod subdirectory
+            tdirs = sorted(glob(seit_dir + '/invmod/*'))
+            grid = crt_grid(
+                elem_file=tdirs[0] + '/grid/elem.dat',
+                elec_file=tdirs[0] + '/grid/elec.dat'
+            )
+            self.grid = grid
+
+            self.load_inversion_results(seit_dir)
+
+        else:
+            # load/assign grid
+            if 'grid' in kwargs:
+                self.grid = kwargs.get('grid')
+            elif 'elem_file' in kwargs and 'elec_file' in kwargs:
+                grid = crt_grid()
+                grid.load_grid(
+                    kwargs['elem_file'],
+                    kwargs['elec_file'],
+                )
+                self.grid = grid
+            else:
+                raise Exception(
+                    'You must provide either a grid instance or ' +
+                    'elem_file/elec_file file paths'
+                )
 
         crt_data_dir = kwargs.get('crt_data_dir', None)
         if crt_data_dir is not None:
@@ -182,6 +200,82 @@ class eitMan(object):
         invmod_dir = directory + os.sep + 'invmod'
         os.makedirs(invmod_dir)
         for nr, key in enumerate(sorted(self.tds.keys())):
-            outdir = invmod_dir + os.sep + '{0:02}_{1:.6}'.format(nr, key)
+            outdir = invmod_dir + os.sep + '{0:02}_{1:.6f}'.format(nr, key)
             print(outdir)
             self.tds[key].save_to_tomodir(outdir)
+
+    def load_inversion_results(self, sipdir):
+        """Given an sEIT inversion directory, load inversion results and store
+        the corresponding parameter ids in self.assignments
+
+        Note that all previous data stored in this instance of the eitManager
+        will be overwritten, if required!
+        """
+        # load frequencies and initialize tomodir objects for all frequencies
+        frequency_file = sipdir + os.sep + 'frequencies.dat'
+        frequencies = np.loadtxt(frequency_file)
+        self._init_frequencies(frequencies)
+
+        # cycle through all tomodirs on disc and load the data
+        for nr, (key, item) in enumerate(sorted(self.tds.items())):
+            for label in ('rmag', 'rpha', 'cre', 'cim'):
+                if label not in self.assigments:
+                    self.a[label] = {}
+
+            tdir = sipdir + os.sep + 'invmod' + os.sep + '{:02}_{:.6f}'.format(
+                nr, key) + os.sep
+            print(tdir, tdir)
+
+            rmag_file = sorted(glob(tdir + 'inv/*.mag'))[-1]
+            rmag_data = np.loadtxt(rmag_file, skiprows=1)[:, 2]
+            pid_rmag = item.parman.add_data(rmag_data)
+            self.a['rmag'][key] = pid_rmag
+
+            rpha_file = sorted(glob(tdir + 'inv/*.pha'))[-1]
+            rpha_data = np.loadtxt(rpha_file, skiprows=1)[:, 2]
+            pid_rpha = item.parman.add_data(rpha_data)
+            self.a['rpha'][key] = pid_rpha
+
+            sigma_file = sorted(glob(tdir + 'inv/*.sig'))[-1]
+            sigma_data = np.loadtxt(sigma_file, skiprows=1)
+            pid_cre = item.parman.add_data(sigma_data[:, 0])
+            pid_cim = item.parman.add_data(sigma_data[:, 1])
+            self.a['cre'][key] = pid_cre
+            self.a['cim'][key] = pid_cim
+
+
+    def extract_points(self, label, points):
+        """Extract data points along a given line
+
+        Parameters
+        ----------
+        label: str
+
+        points: Nx2 numpy.ndarray
+            (x, y) pairs
+
+        Returns
+        -------
+
+        """
+        if isinstance(label, str):
+            label = [label, ]
+
+        data_list = []
+        for label_key in label:
+            value_list = {}
+            for key, item in sorted(self.tds.items()):
+                values = item.parman.extract_points(
+                    pid=self.a[label_key][key],
+                    points=points
+                )
+                value_list[key] = values
+            df = pd.DataFrame(value_list)
+            df['x'] = points[:, 0]
+            df['z'] = points[:, 1]
+            df['key'] = label_key
+            data_list.append(df)
+        df_all = pd.concat(data_list)
+
+        return df_all
+
