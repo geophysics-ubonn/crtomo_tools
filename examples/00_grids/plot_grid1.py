3452a7630bd10adfb0afc96959d7e2d5a70949ea
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2018-10-01 15:32:51 +0200

    work on examples

--- examples/00_grids/plot_grid1.py
+++ examples/00_grids/plot_grid1.py
@@ -1,16 +1,27 @@
 #!/usr/bin/env python
 # *-* coding: utf-8 *-*
 """
-This is my example script
-=========================
+Creating and handling grids
+===========================
 
-This example doesn't do much, it just makes a simple plot
 """
-
+###############################################################################
+# The toplevel crtomo import suffices for most tasks
 import crtomo
-crtomo
 
+###############################################################################
+# Create simple surface grids with this wrapper
 grid = crtomo.crt_grid.create_surface_grid(nr_electrodes=10, spacing=1.5)
-
 grid.plot_grid()
+# number the electrodes (useful for numerical studies)
+grid.plot_grid(plot_electrode_numbers=True)
 
+# save this grid to disc
+grid.save_elem_file('elem.dat')
+grid.save_elec_file('elec.dat')
+
+
+###############################################################################
+# Grid can be read from disk:
+grid1 = crtomo.crt_grid('elem.dat', 'elec.dat')
+print(grid1)
--- examples/01_modelling/plot_potentials.py
+++ examples/01_modelling/plot_potentials.py
@@ -1,46 +1,53 @@
 #!/usr/bin/env python3
 # *-* coding: utf-8 *-*
 """
-This is my example script
-=========================
+Plot a potential distribution, computed with CRMod
+==================================================
 
-This example doesn't do much, it just makes a simple plot
+
 """
+###############################################################################
+# create a tomodir object
 import crtomo
-grid = crtomo.crt_grid('grid_rhizotron/elem.dat', 'grid_rhizotron/elec.dat')
-
+grid = crtomo.crt_grid('grid_surface/elem.dat', 'grid_surface/elec.dat')
 td = crtomo.tdMan(grid=grid)
-td.crmod_cfg['write_pots'] = 'T'
-td.crmod_cfg['2D'] = '0'
-td.crmod_cfg['fictitious_sink'] = 'T'
 
+###############################################################################
+# define configurations
 import numpy as np
 td.configs.add_to_configs(
     np.array((
-        (6, 26, 1, 2),
-        (1, 3, 7, 5),
+        (1, 10, 5, 7),
+        (1, 3, 10, 7),
     ))
 )
 
-# import IPython; IPython.embed()
-# exit()
-# td.add_homogeneous_model(100, 0)
-r = td.load_rho_file('grid_rhizotron/rho_root.dat')
-td.register_forward_model(*r)
+###############################################################################
+# add a homogeneous forward model
+td.add_homogeneous_model(100, 0)
 
+###############################################################################
+# compute FEM solution using CRMod
 td.model(potentials=True)
 
+
+###############################################################################
+# plot first quadrupole
 pot_mag, pot_pha = td.get_potential(0)
 
+# add node data to the parameter manager
 nid = td.nodeman.add_data(pot_mag)
+# TODO: is this the phase, or imaginary part of the potential?
 nid_pha = td.nodeman.add_data(pot_pha)
 
+# plot
 import pylab as plt
-fig, axes = plt.subplots(1, 2, figsize=(16 / 2.54, 8 / 2.54))
+fig, axes = plt.subplots(2, 1, figsize=(16 / 2.54, 8 / 2.54))
 
 ax = axes[0]
 td.plot.plot_nodes_pcolor_to_ax(
-    ax, nid,
+    ax,
+    nid,
     plot_colorbar=True,
 )
 # td.plot.plot_nodes_contour_to_ax(
@@ -49,10 +56,12 @@ td.plot.plot_nodes_pcolor_to_ax(
 
 # )
 ax.set_aspect('equal')
+
 ax = axes[1]
 td.plot.plot_nodes_pcolor_to_ax(
-    ax, nid_pha,
+    ax,
+    nid_pha,
     plot_colorbar=True,
 )
 ax.set_aspect('equal')
-fig.savefig('test.pdf')
+fig.savefig('test.pdf', bbox_inches='tight')
