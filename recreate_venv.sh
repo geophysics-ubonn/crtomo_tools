a106c7de6a26972a526badc67dfe489c6980bc80
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2018-10-17 21:52:34 +0200

    helper script

--- recreate_venv.sh
+++ recreate_venv.sh
@@ -6,4 +6,5 @@ rmvirtualenv crtomo
 mkvirtualenv --python /usr/bin/python3 crtomo
 pip install -r requirements.txt
 pip install -r requirements_doc.txt
+pip install -r requirements_dev.txt
 python setup.py develop
