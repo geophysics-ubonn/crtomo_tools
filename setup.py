0bb83a33a64d2c84b8e1fe8f202bb6aff1548494
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2019-09-29 13:44:29 +0200

    fix version numbering scheme and switch to 0.2.2-dev

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import glob
 scripts = glob.glob('src/*.py')
 
 version_short = '0.2'
-version_long = '0.2.2'
+version_long = '0.2.2-dev'
 
 # generate entry points
 entry_points = {'console_scripts': []}
