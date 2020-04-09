48dc621ec498f814ce441e7c3ebd963ff522a93f
Valentin Michels <michels@geo.uni-bonn.de>
2020-04-09 08:33:12 +0200

    Updated postprocessing example with new colormaps and parameters

--- examples/03_inversion_postprocessing/plot_02_inversion_results.py
+++ examples/03_inversion_postprocessing/plot_02_inversion_results.py
@@ -1,3 +1,4 @@
+#%%
 #!/usr/bin/env python3
 # *-* coding: utf-8 *-*
 """
@@ -6,6 +7,7 @@ Plot inversion results from a tomodir
 
 """
 import crtomo
+import numpy as np
 tdm = crtomo.tdMan(tomodir='tomodir')
 
 ###############################################################################
@@ -14,35 +16,108 @@ tdm = crtomo.tdMan(tomodir='tomodir')
 tdm.show_parset(tdm.a['inversion']['rmag'][-1])
 tdm.show_parset(tdm.a['inversion']['rpha'][-1])
 ###############################################################################
-# Let's do this the nice way (for phase values only)
+# Let's do this the nice way: We want to plot the magnitude, real and imaginary
+# part of the complex conductivity as well as the phase into one plot result.
+
 import matplotlib.pylab as plt
 # extract parameter set ids
+pid_rmag = tdm.a['inversion']['rmag'][-1]
 pid_pha = tdm.a['inversion']['rpha'][-1]
+pid_cre = tdm.a['inversion']['cre'][-1]
+pid_cim = tdm.a['inversion']['cim'][-1]
 
-fig, ax = plt.subplots(1, 1, figsize=(16 / 2.54, 7 / 2.54))
+# Note that we can switch out the resistivity magnitude with the conductivity
+# magnitude by accessing the parset and taking the inverse values.
+rmag = tdm.parman.parsets[pid_rmag]
+cmag = 1/rmag
+
+# Subsequently, we can assign a new parameter set id (cmag) for the 
+# conductivity magnitude data.
+pid_cmag = tdm.parman.add_data(cmag)
+
+# Our four datasets can now be plotted using the built-in CRTomo-function 
+# "plot_elements_to_ax". We can use the "converter-parameter" to convert
+# the conductivity data to the log10-scale:
+fig, axes = plt.subplots(4, 1, figsize=(12 / 2.54, 16 / 2.54), sharex=True)
+
+ax = axes[0]
+tdm.plot.plot_elements_to_ax(
+    cid=pid_cmag,
+    ax=ax,
+    plot_colorbar=True,
+    cmap_name='turbo',
+    xmin=-0.0,
+    xmax=3.5,
+    zmin=-1.0,
+    cblabel=r'$log_{10}(|\sigma^*| [S/m])$',
+    converter=np.log10,
+)
+ax.get_xaxis().set_visible(False)
+ax.set_ylabel('z [m]')
+
+ax = axes[1]
 tdm.plot.plot_elements_to_ax(
     cid=pid_pha,
     ax=ax,
     plot_colorbar=True,
-    cmap_name='jet',
+    cmap_name='CMRmap',
     xmin=-0.0,
     xmax=3.5,
     zmin=-1.0,
     cblabel=r'$\phi [mrad]$',
 )
+ax.get_xaxis().set_visible(False)
+ax.set_ylabel('z [m]')
+
+ax = axes[2]
+tdm.plot.plot_elements_to_ax(
+    cid=pid_cre,
+    ax=ax,
+    plot_colorbar=True,
+    cmap_name='turbo',
+    xmin=-0.0,
+    xmax=3.5,
+    zmin=-1.0,
+    cblabel=r"$log_{10}(\sigma'~ [S/m])$",
+    converter=np.log10,
+)
+ax.get_xaxis().set_visible(False)
+ax.set_ylabel('z [m]')
+
+ax = axes[3]
+tdm.plot.plot_elements_to_ax(
+    cid=pid_cim,
+    ax=ax,
+    plot_colorbar=True,
+    cmap_name='gnuplot2_r',
+    xmin=-0.0,
+    xmax=3.5,
+    zmin=-1.0,
+    cblabel=r"$log_{10}(\sigma''~ [S/m])$",
+    converter=np.log10,
+)
+ax.set_ylabel('z [m]')
+ax.set_xlabel('x [m]')
+
+fig.tight_layout()
+
+# For clarity reasons, it is advisable to plot the parameters in different 
+# colors - here, we used the cmaps 'turbo' for Magnitude and Real part of
+# the conductivity, 'CMRmap' for the phase and 'gnuplot2_r' 
+# for the imaginary part of the conductivity.
 
 ###############################################################################
-# Plot values can be transformed (converted) on the fly using a
-# converter-function, provided using the converter parameter.
-# As an example, lets change the sign of the phase values (note that we also
-# swap the colormap)
+# With the converter parameter, measurement data can be changed on the fly with
+# any given function.
+# As an example, lets change the sign of the phase values with the function
+# "converter_change_sign" (note that we also swap the colormap).
 from crtomo.plotManager import converter_change_sign
 
 fig, ax = plt.subplots(1, 1, figsize=(16 / 2.54, 7 / 2.54))
 tdm.plot.plot_elements_to_ax(
     cid=tdm.inv_last_rpha_parset(),
     ax=ax,
-    cmap_name='jet_r',
+    cmap_name='CMRmap_r',
     plot_colorbar=True,
     xmin=-0.0,
     xmax=3.5,
@@ -57,41 +132,10 @@ tdm.plot.plot_elements_to_ax(
 # from crtomo.plotManager import converter_log10_to_lin
 
 ###############################################################################
-# Lets plot real and imaginary parts of the conductivity as log10 values
-# Note that we now use the numpy.log10 function as a converter
-import numpy as np
-
-fig, axes = plt.subplots(2, 1, figsize=(16 / 2.54, 9 / 2.54))
-ax = axes[0]
-
-tdm.plot.plot_elements_to_ax(
-    cid=tdm.inv_last_cre_parset(),
-    ax=ax,
-    cmap_name='viridis',
-    plot_colorbar=True,
-    xmin=-0.0,
-    xmax=3.5,
-    zmin=-1.0,
-    cblabel=r"$log_{10}(\sigma' [S/m])$",
-    converter=np.log10,
-)
-
-ax = axes[1]
-
-tdm.plot.plot_elements_to_ax(
-    cid=tdm.inv_last_cim_parset(),
-    ax=ax,
-    cmap_name='viridis',
-    plot_colorbar=True,
-    xmin=-0.0,
-    xmax=3.5,
-    zmin=-1.0,
-    cblabel=r"$log_{10}(\sigma' [S/m])$",
-    converter=np.log10,
-)
-
-###############################################################################
-# Create a depth cut at x = 4 m, down to 2 m depth
+# Sometimes, it can also be of interest to only look at the slice or area 
+# of the data. This can be achieved with the extract_along_line or 
+# extract_polygon_area functions.
+# Here, we create a depth cut at x = 4 m, down to 2 m depth.
 pid_pha = tdm.a['inversion']['rpha'][-1]
 results = tdm.parman.extract_along_line(pid_pha, [4, -2], [4, 0])
 # x y value
@@ -104,7 +148,7 @@ ax.set_xlabel(r'$-\phi [mrad]$')
 ax.set_ylabel('depth [m]')
 
 ###############################################################################
-# Resolution assessment
+# Resolution assessment:
 # Note that you must explicitly tell CRTomo to compute these measures. This can
 # be done using the mswitch: (note that the following line only works if you
 # actually invert -- here we load from an existing tomodir!) ::
@@ -117,7 +161,7 @@ ax = axes[0]
 tdm.plot.plot_elements_to_ax(
     cid=tdm.a['inversion']['resm'],
     ax=ax,
-    cmap_name='viridis',
+    cmap_name="turbo",
     plot_colorbar=True,
     xmin=-0.0,
     xmax=3.5,
@@ -131,12 +175,12 @@ ax = axes[1]
 tdm.plot.plot_elements_to_ax(
     cid=tdm.a['inversion']['l1_dw_log10_norm'],
     ax=ax,
-    cmap_name='viridis',
+    cmap_name="turbo",
     plot_colorbar=True,
     xmin=-0.0,
     xmax=3.5,
     zmin=-1.0,
-    cblabel=r"$L1 Coverage$",
+    cblabel=r"$log_{10}(L1 Coverage)$",
     # converter=np.log10,
 )
 
@@ -145,11 +189,15 @@ ax = axes[2]
 tdm.plot.plot_elements_to_ax(
     cid=tdm.a['inversion']['l2_dw_log10_norm'],
     ax=ax,
-    cmap_name='viridis',
+    cmap_name="turbo",
     plot_colorbar=True,
     xmin=-0.0,
     xmax=3.5,
     zmin=-1.0,
-    cblabel=r"$L2 Coverage$",
+    cblabel=r"$log_{10}(L2 Coverage)$",
     # converter=np.log10,
 )
+
+
+
+# %%
