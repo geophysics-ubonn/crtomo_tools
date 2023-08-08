d3cff9aad7e73b4097e9550f4408a8e9c25fefda
Maximilian Weigand <mweigand@mweigand.net>
2023-08-08 14:59:48 +0200

    fix the td_residuals command

--- src/td_residuals.py
+++ src/td_residuals.py
@@ -4,6 +4,7 @@ import numpy as np
 import os
 import matplotlib.pyplot as plt
 import matplotlib as mpl
+import pandas as pd
 # from mpl_toolkits.axes_grid1 import AxesGrid
 
 
@@ -55,7 +56,8 @@ def shiftedColorMap(cmap, start=0, midpoint=0.5, stop=1.0, name='shiftedcmap'):
         cdict['alpha'].append((si, a, a))
 
     newcmap = mpl.colors.LinearSegmentedColormap(name, cdict)
-    plt.register_cmap(cmap=newcmap)
+
+    # plt.register_cmap(cmap=newcmap)
 
     return newcmap
 
@@ -66,7 +68,7 @@ def read_lastmodfile(directory):
     """
     filename = '{0}/exe/inv.lastmod'.format(directory)
     # filename HAS to exist. Otherwise the inversion was not finished
-    if(not os.path.isfile(filename)):
+    if not os.path.isfile(filename):
         return None
 
     linestring = open(filename, 'r').readline().strip()
@@ -90,54 +92,60 @@ def open_inv():
 def main():
     data = open_data()
     inv = open_inv()
-    if(
-            (data[:, 0].all() - inv[:, 0].all() == 0) and
-            (data[:, 1].all() - inv[:, 1].all() == 0)):
+    # import IPython
+    # IPython.embed()
+    ab_are_equal = (data[:, 0] - inv[:, 0] == 0).all()
+    mn_are_equal = (data[:, 1] - inv[:, 1] == 0).all()
+    if ab_are_equal and mn_are_equal:
         print('Dipoles are equal')
-        source = []
-        receiver = []
-        for AB, MN, rho, pha in data:
-            s = ((AB % 1e4) - (round(AB / 1e4))) / 2 + round(AB / 1e4)
-            source.append(s)
-            r = ((round(MN / 1e4)) - (MN % 1e4)) / 2 + (MN % 1e4)
-            receiver.append(r)
-        mag_res = np.column_stack(
-            (source, receiver, data[:, 2] - inv[:, 2], )
-        )
+        rdata = pd.DataFrame((data[:, 0] / 1e4).astype(int), columns=['a', ])
+        rdata['b'] = (data[:, 0] % 1e4).astype(int)
+        rdata['m'] = (data[:, 1] / 1e4).astype(int)
+        rdata['n'] = (data[:, 1] % 1e4).astype(int)
+        rdata['ab_pos'] = (rdata['b'] - rdata['a']) / 2 + rdata['a']
+        rdata['mn_pos'] = (rdata['n'] - rdata['m']) / 2 + rdata['m']
+        rdata['diff_res'] = data[:, 2] - inv[:, 2]
+        rdata['diff_pha'] = data[:, 3] - inv[:, 3]
+        rdata['diff_res_abs'] = np.abs(rdata['diff_res'])
+        rdata['diff_pha_abs'] = np.abs(rdata['diff_pha'])
 
-        vmin = min(mag_res[:, 2])
-        vmax = max(mag_res[:, 2])
+        # import IPython
+        # IPython.embed()
+
+        vmin = min(rdata['diff_res'])
+        vmax = max(rdata['diff_res'])
         shrunk_cmap = shiftedColorMap(
-            mpl.cm.coolwarm, midpoint=1 - vmax / (vmax + abs(vmin))
+            mpl.cm.coolwarm,
+            midpoint=1 - vmax / (vmax + abs(vmin)),
+            name='map1',
         )
         fig, ax = plt.subplots()
-        sc = ax.scatter(mag_res[:, 0],
-                        mag_res[:, 1],
-                        c=mag_res[:, 2],
-                        # s=7,
-                        edgecolor='k',
-                        linewidth=.1,
-                        cmap=shrunk_cmap
-                        )
+        sc = ax.scatter(
+            rdata['ab_pos'],
+            rdata['mn_pos'],
+            c=rdata['diff_res'],
+            # s=7,
+            edgecolor='k',
+            linewidth=.1,
+            cmap=shrunk_cmap
+        )
         plt.colorbar(sc)
         ax.set_xlabel('source position [m]')
         ax.set_ylabel('receiver position [m]')
         fig.tight_layout()
         fig.savefig('residuals_mag.png', dpi=300)
-        pha_res = np.column_stack(
-            (source, receiver, data[:, 3] - inv[:, 3], )
-        )
 
-        vmin = min(pha_res[:, 2])
-        vmax = max(pha_res[:, 2])
+        vmin = min(rdata['diff_pha'])
+        vmax = max(rdata['diff_pha'])
         shrunk_cmap = shiftedColorMap(
-            mpl.cm.coolwarm, midpoint=1 - vmax / (vmax + abs(vmin))
+            mpl.cm.coolwarm, midpoint=1 - vmax / (vmax + abs(vmin)),
+            name='map2',
         )
         fig, ax = plt.subplots()
         sc = ax.scatter(
-            pha_res[:, 0],
-            pha_res[:, 1],
-            c=pha_res[:, 2],
+            rdata['ab_pos'],
+            rdata['mn_pos'],
+            c=rdata['diff_pha'],
             # s=7,
             edgecolor='k',
             linewidth=.1,
@@ -148,6 +156,13 @@ def main():
         ax.set_ylabel('receiver position [m]')
         fig.tight_layout()
         fig.savefig('residuals_pha.png', dpi=300)
+
+        # save residuals to file
+        rdata.sort_values('diff_res_abs', ascending=False).to_csv(
+            'residuals.csv',
+            sep='\t',
+            float_format='%.3f',
+        )
 
 
 if __name__ == '__main__':
