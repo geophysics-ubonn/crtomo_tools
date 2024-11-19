e550c718de6642956685bbe2a88785170b537ebe
jasperlendla <19495293+jasperlendla@users.noreply.github.com>
2024-11-19 13:39:12 +0100

    Source the virtualenvwrapper.sh script using which to cope with different installation directories.

--- build_documentation_ci.sh
+++ build_documentation_ci.sh
@@ -2,7 +2,7 @@
 # Script to build the documentation, to be called from any CI runner
 # such as Github actions
 
-source /usr/share/virtualenvwrapper/virtualenvwrapper.sh
+source $(which virtualenvwrapper.sh)
 workon crtomo
 cd doc
 make html
