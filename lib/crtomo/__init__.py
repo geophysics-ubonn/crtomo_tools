452987c05d5e1caf608cf42e792f77f0d5c47514
Maximilian Weigand <mweigand@mweigand.net>
2025-08-04 12:39:42 +0200

    rework global includes

--- lib/crtomo/__init__.py
+++ lib/crtomo/__init__.py
@@ -1,6 +1,7 @@
 from crtomo.tdManager import tdMan
 from crtomo.tdManager import noise_model
 from crtomo.grid import crt_grid
+from crtomo.grid import mesh
 from crtomo.plotManager import plotManager as pltMan
 from crtomo.parManager import ParMan as ParMan
 from crtomo.eitManager import eitMan
@@ -9,10 +10,14 @@ from crtomo.status import td_is_finished
 from crtomo.status import seitdir_is_finished
 
 __all__ = [
-    'pltMan', 'tdMan',
-    'eitMan', 'crt_grid',
+    'pltMan',
+    'tdMan',
+    'eitMan',
+    'mesh',
+    'crt_grid',
     'noise_model',
     'ConfigManager',
     'td_is_finished',
     'seitdir_is_finished',
+    'ParMan',
 ]
