3d04ea5c73c37d266e5b6c769ec439dc95a185a0
Maximilian Weigand <mweigand@mweigand@uni-bonn.de>
2026-05-29 09:01:23 +0200

    fix year in copyright

--- doc/conf.py
+++ doc/conf.py
@@ -120,7 +120,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'crtomo tools'
-copyright = u'2017-2023, CRTomo contributors'
+copyright = u'2017-2026, CRTomo contributors'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
